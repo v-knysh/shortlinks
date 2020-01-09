139241c56c51a241bc4f497b937536ad3416223a
Vladyslav Knysh <knyshv2412@gmail.com>
2020-01-09 15:04:17 +0200

    used single serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.utils.crypto import get_random_string
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, Serializer
 
@@ -5,18 +8,35 @@ from api.models import Link
 
 
 class LinkSerializer(ModelSerializer):
-    requires_context = True
-    last_action = serializers.DateTimeField(default=None)
-    count = serializers.IntegerField(default=0)
+    last_action = serializers.DateTimeField(default=None, read_only=False)
+    count = serializers.IntegerField(default=0, read_only=False)
 
     class Meta:
         model = Link
         fields = "__all__"
+        read_only_fields = [
+            "last_action",
+            "count",
+            "short_url",
+            "expiration_date",
+            "created",
+            "author_ip",
+            "is_active",
+        ]
 
     def to_representation(self, instance):
         host = self.context['request'].get_host()
         instance.short_url = f"http://{host}/{instance.short_url}"
         return super().to_representation(instance)
 
-class LinkAddSerializer(Serializer):
-    redirect_location = serializers.URLField(max_length=256, allow_blank=False)
+    def create(self, validated_data):
+        user_ip = self.context['request'].user_ip
+        short_url = get_random_string(length=6)
+        link = Link(
+            short_url=short_url,
+            created=datetime.now(),
+            author_ip=user_ip,
+            redirect_location=validated_data["redirect_location"],
+        )
+        link.save()
+        return link
--- api/views.py
+++ api/views.py
@@ -1,15 +1,12 @@
 import datetime
-import random
 
-from django.db import IntegrityError
 from django.db.models import Max, Count
 from django.http import HttpResponse
 from django.shortcuts import redirect, get_object_or_404
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from api.models import Link, LinkAction
-from api.serializers import LinkSerializer, LinkAddSerializer
+from api.serializers import LinkSerializer
 
 
 def unwrap_shortlink(request, short_url):
@@ -28,48 +25,10 @@ def index(request):
     return HttpResponse("<h1>Hello!</h1>")
 
 
-class LinksViewSet(viewsets.ViewSet):
-    def list(self, request):
-        users_links_queryset = self._base_queryset(request.user_ip)
-        serializer = LinkSerializer(users_links_queryset, many=True, context={'request': request})
-        return Response(serializer.data, )
-
-    def retrieve(self, request, pk=None):
-        queryset = self._base_queryset(request.user_ip)
-        link_query = get_object_or_404(queryset, id=pk)
-        serializer = LinkSerializer(link_query, context={'request': request})
-        return Response(serializer.data)
-
-    def _base_queryset(self, user_ip):
-        return Link.objects.filter(author_ip=user_ip).annotate(count=Count("actions")).annotate(last_action=Max("actions__action_datetime"))
-
-    def create(self, request):
-        serializer = LinkAddSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=400)
-        redirect_location = serializer.validated_data['redirect_location']
-        for i in range(10):
-            try:
-                short_url = random_string()
-                link = Link(
-                    short_url=short_url,
-                    created=datetime.datetime.now(),
-                    author_ip=request.user_ip,
-                    redirect_location=redirect_location,
-                )
-                link.save()
-            except IntegrityError:
-                pass
-            else:
-                break
-        else:
-            return Response("Something went wrong", status=500)
-        return Response(LinkSerializer(link).data)
-
-
-def random_string():
-    s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
-    res = []
-    for i in range(6):
-        res.append(random.choice(s))
-    return "".join(res)
+class LinksViewSet(viewsets.ModelViewSet):
+    serializer_class = LinkSerializer
+    
+    def get_queryset(self):
+        user_ip = self.request.user_ip
+        return Link.objects.filter(author_ip=user_ip).annotate(count=Count("actions")).annotate(
+            last_action=Max("actions__action_datetime"))
